8af6067ccb9bcda1ec439ffc20ea8cf37ef93472
ExileNoir <Deseure.s@gmail.com>
2018-11-12 18:18:13 +0100

    Mr Noir: EMPprovidor OK, Persistence OK

---
+++ src/main/java/be/intecbrussel/blogProject/dao/EMProvidor.java
@@ -0,0 +1,58 @@
+package be.intecbrussel.blogProject.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Date;
+
+public class EMProvidor {
+
+    // Variables
+    private static final String DB_SCHEMA = "TheBlog";
+    private static final boolean DEBUG = true;
+    private static EMProvidor ourInstance = new EMProvidor();
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+
+    // Constructor
+    private EMProvidor() {
+
+    }
+
+    // Methods
+    public static EMProvidor getInstance() {
+        return ourInstance;
+    }
+
+    /**
+     * Creates an EntityManagerFactory
+     */
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory(DB_SCHEMA);
+        }
+        if (DEBUG) {
+            System.out.println("Entity Manager Factory created on " + new Date());
+        }
+        return emf;
+    }
+
+    /**
+     * Creates an EntityManager via
+     *
+     * @see EMProvidor#getEntityManagerFactory()
+     */
+    public static EntityManager getEntityManager() {
+        return getEntityManagerFactory().createEntityManager();
+    }
+
+    public void closeEmf() {
+        if (emf.isOpen() || emf != null) {
+            emf.close();
+        }
+        emf = null;
+        if (DEBUG) {
+            System.out.println("Entity Manager Factory closed at: " + new Date());
+        }
+    }
+}
