b62c1e587130feec1773c3b27a07c76103adb493
Nina <asgoodasninagold@gmail.com>
2018-11-16 13:58:57 +0100

    not sure about listener.. added exceptions package

---
+++ src/main/java/be/intecbrussel/blogProject/exceptions/InvalidContextAttributeException.java
@@ -0,0 +1,11 @@
+package be.intecbrussel.blogProject.exceptions;
+
+public class InvalidContextAttributeException extends Exception {
+
+    public InvalidContextAttributeException() {
+    }
+
+    public InvalidContextAttributeException(String message) {
+        super(message);
+    }
+}
