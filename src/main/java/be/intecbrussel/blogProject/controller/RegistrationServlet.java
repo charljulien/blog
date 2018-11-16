18799580bab6c6af60646fbef4ff08176e1fed29
ExileNoir <Deseure.s@gmail.com>
2018-11-16 15:10:35 +0100

    small adjust on static var

--- src/main/java/be/intecbrussel/blogProject/controller/RegistrationServlet.java
+++ src/main/java/be/intecbrussel/blogProject/controller/RegistrationServlet.java
@@ -1,18 +1,19 @@
 package be.intecbrussel.blogProject.controller;
 
-import be.intecbrussel.blogProject.beans.UserBean;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/Registration")
 public class RegistrationServlet extends HttpServlet {
-    protected void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-        request.getRequestDispatcher("WEB-INF/forms/registration.jsp").forward(request, response);
+
+    private static final String REGISTRATION_PAGE = "WEB-INF/forms/registration.jsp";
+
+    @Override
+    public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        request.getRequestDispatcher(REGISTRATION_PAGE).forward(request, response);
     }
 }
