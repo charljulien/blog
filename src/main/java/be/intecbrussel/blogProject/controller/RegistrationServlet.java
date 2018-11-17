24d5b0e4ff32a9701540c886448dad3c26c6eb05
Charles <charljulien@gmail.com>
2018-11-17 11:12:59 +0100

    Registration servlet upgraded

--- src/main/java/be/intecbrussel/blogProject/controller/RegistrationServlet.java
+++ src/main/java/be/intecbrussel/blogProject/controller/RegistrationServlet.java
@@ -21,15 +21,25 @@ import java.util.List;
 @WebServlet("/Registration")
 public class RegistrationServlet extends HttpServlet {
 
-    List<String> items;
-    UserService userService;
-    UserBean userBean;
-
-//    protected void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-//        request.getRequestDispatcher("WEB-INF/forms/registration.jsp").forward(request, response);
-
+    private static final String FIRST_NAME = "firstName";
+    private static final String LAST_NAME = "lastName";
+    private static final String EMAIL = "email";
+    private static final String STREET = "street";
+    private static final String HOUSE_NUMBER = "houseNumber";
+    private static final String CITY = "city";
+    private static final String ZIP_CODE = "zipCode";
+    private static final String USER_NAME = "userName";
+    private static final String PASSWORD = "password";
 
     private static final String REGISTRATION_PAGE = "WEB-INF/forms/registration.jsp";
+    private static final String REGISTRATION_ERROR = "WEB-INF/forms/ERRORregistration.jsp";
+    private static final String BLOG_CENTRAL_PAGE = "WEB-INF/theBlog/combinationsAkaPages/blogCentral.jsp";
+
+    private static final String USER_SERVICE = "userService";
+
+    private List<String> items;
+    private UserService userService;
+    private UserBean userBean;
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -53,14 +63,14 @@ public class RegistrationServlet extends HttpServlet {
 
         //if he did shit, he goes to the error page, that's the spirit
         if (isEmpty) {
-            request.getRequestDispatcher("WEB-INF/forms/ERRORregistration.jsp");
+            request.getRequestDispatcher(REGISTRATION_ERROR);
         }
         //otherwise he goes to main page and his profile is created
         else {
             userBean = configureUserBean(userBean, request);
-            userService = (UserService) getServletContext().getAttribute("userService");
+            userService = (UserService) getServletContext().getAttribute(USER_SERVICE);
             userService.saveUserToDB(userBean);
-            request.getRequestDispatcher("WEB-INF/theBlog/combinationsAkaPages/blogCentral.jsp");
+            request.getRequestDispatcher(BLOG_CENTRAL_PAGE);
         }
     }
 
@@ -69,15 +79,15 @@ public class RegistrationServlet extends HttpServlet {
      */
     //methode to create a list of parameters
     public List<String> getItems(HttpServletRequest request) {
-        String firstName = request.getParameter("firstName");
-        String lastName = request.getParameter("lastName");
-        String email = request.getParameter("email");
-        String street = request.getParameter("street");
-        String houseNumber = request.getParameter("houseNumber");
-        String city = request.getParameter("city");
-        String zipCode = request.getParameter("zipCode");
-        String userName = request.getParameter("userName");
-        String password = request.getParameter("password");
+        String firstName = request.getParameter(FIRST_NAME);
+        String lastName = request.getParameter(LAST_NAME);
+        String email = request.getParameter(EMAIL);
+        String street = request.getParameter(STREET);
+        String houseNumber = request.getParameter(HOUSE_NUMBER);
+        String city = request.getParameter(CITY);
+        String zipCode = request.getParameter(ZIP_CODE);
+        String userName = request.getParameter(USER_NAME);
+        String password = request.getParameter(PASSWORD);
 
         items.add(firstName);
         items.add(lastName);
@@ -97,15 +107,15 @@ public class RegistrationServlet extends HttpServlet {
      */
     //methode to create user with valid parameters
     public UserBean configureUserBean(UserBean userBean, HttpServletRequest request) {
-        userBean.setFirstname(request.getParameter("firstName"));
-        userBean.setLastName(request.getParameter("lastName"));
-        userBean.setEmail(request.getParameter("email"));
-        userBean.setStreet(request.getParameter("street"));
-        userBean.setHouseNr(request.getParameter("houseNumber"));
-        userBean.setCity(request.getParameter("city"));
-        userBean.setZipCode(request.getParameter("zipCode"));
-        userBean.setUserName(request.getParameter("userName"));
-        userBean.setPassword(request.getParameter("password"));
+        userBean.setFirstname(request.getParameter(FIRST_NAME));
+        userBean.setLastName(request.getParameter(LAST_NAME));
+        userBean.setEmail(request.getParameter(EMAIL));
+        userBean.setStreet(request.getParameter(STREET));
+        userBean.setHouseNr(request.getParameter(HOUSE_NUMBER));
+        userBean.setCity(request.getParameter(CITY));
+        userBean.setZipCode(request.getParameter(ZIP_CODE));
+        userBean.setUserName(request.getParameter(USER_NAME));
+        userBean.setPassword(request.getParameter(PASSWORD));
 
         return userBean;
     }
