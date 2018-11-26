80ac31677038bee889b0444517f9fe4e34da7781
Charles <charljulien@gmail.com>
2018-11-26 14:22:20 +0100

    user profile servlet to be tested

--- src/main/java/be/intecbrussel/blogProject/controller/AuthorHomeServlet.java
+++ src/main/java/be/intecbrussel/blogProject/controller/AuthorHomeServlet.java
@@ -3,6 +3,8 @@ package be.intecbrussel.blogProject.controller;
 import be.intecbrussel.blogProject.beans.BlogPostBean;
 import be.intecbrussel.blogProject.beans.UserBean;
 import be.intecbrussel.blogProject.dao.BlogPostDAO;
+import be.intecbrussel.blogProject.listeners.AppContextListener;
+import be.intecbrussel.blogProject.service.implementations.BlogPostService;
 import be.intecbrussel.blogProject.service.interfaces.BlogPostServiceInterface;
 
 import javax.servlet.ServletException;
@@ -23,17 +25,25 @@ import static be.intecbrussel.blogProject.controller.UserLoginServlet.USER_BEAN;
 
 @WebServlet("/AuthorPage")
 public class AuthorHomeServlet extends HttpServlet {
-    private BlogPostServiceInterface blogPostServiceInterface;
+    private BlogPostServiceInterface blogPostService;
     private BlogPostDAO blogPostDAO = new BlogPostDAO();
 
     private static final String AUTHOR_PAGE = "WEB-INF/theBlog/fullPages/authorPage.jsp";
     private static final String USER_NAME = "userName";
 
     @Override
+    public void init() throws ServletException {
+        blogPostService = (BlogPostService) getServletContext().getAttribute(AppContextListener.BLOG_SERVICE);
+        if (blogPostService == null) {
+            throw new ServletException("BlogPostService not available");
+        }
+    }
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         String userName = (String) session.getAttribute(USER_NAME);
-        List<BlogPostBean> blogPostByUser = blogPostDAO.getBlogsByPredefinedUser(USER_NAME);
+        List<BlogPostBean> blogPostByUser = blogPostDAO.getBlogsByPredefinedUser(userName);
         System.out.println(blogPostByUser);
         request.setAttribute("blogPostByUser", blogPostByUser);
         request.getRequestDispatcher(AUTHOR_PAGE).forward(request,response);
